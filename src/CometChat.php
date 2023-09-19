86c7ca34b69a3dd57a8ff3c3321fc6a22ef15f15
Clay Bitner <claybitner@gmail.com>
2023-09-19 12:22:13 -0600

    Create CometChat.php

---
+++ src/CometChat.php
@@ -0,0 +1,501 @@
+<?php
+
+namespace DigitalIndoorsmen\LaravelCometChatAPI;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Message;
+use function json_decode;
+use function mb_strtolower;
+
+class CometChat
+{
+    private static $apiKey;
+    private static $apiRegion;
+    private static $apiVersion;
+    private static $appId;
+    public static  $onBehalfOf;
+
+    /**
+     * CometChat Api constructor.
+     */
+    public function __construct($onBehalfOf=null)
+    {
+        self::$apiKey = config('cometchat.api_key');
+        self::$apiRegion = config('cometchat.api_region');
+        self::$apiVersion = config('cometchat.api_version');
+        self::$appId = mb_strtolower(config('cometchat.app_id'));
+        self::$onBehalfOf = $onBehalfOf;
+    }
+
+    /**
+     * Create a Group
+     *
+     * @param $guid
+     * @param $name
+     * @param string $password
+     * @param string $type
+     * @param null $icon
+     * @param null $description
+     * @param array  $metadata
+     * @param null $owner
+     * @param array  $tags
+     * @param array  $members
+     * @param array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function createGroup(
+        $guid,
+        $name,
+        $password,
+        $type='private',
+        $icon = null,
+        $description = null,
+        $metadata = [],
+        $owner = null,
+        $tags = [],
+        $members = [],
+        $parameters = []
+    ) {
+        $path = '/groups';
+        $method = 'POST';
+        $parameters['guid'] = $guid;
+        $parameters['name'] = $name;
+        $parameters['password'] = $password;
+        $parameters['type'] = $type;
+        if ($icon) $parameters['icon'] = $icon;
+        if ($description) $parameters['description'] = $description;
+        if ($metadata) $parameters['metadata'] = $metadata;
+        if ($owner) $parameters['owner'] = $owner;
+        if ($tags) $parameters['tags'] = $tags;
+        if ($members) $parameters['members'] = $members;
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+
+    /**
+     * List Groups
+     *
+     * @param null $searchKey
+     * @param int    $page
+     * @param array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function listGroups(
+        $searchKey = null,
+        $page = 0,
+        $parameters = []
+    ) {
+        $path = '/groups';
+        $method = 'GET';
+        if ($searchKey) $parameters['searchKey'] = $searchKey;
+        $parameters['page'] = $page;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Get Group
+     *
+     * @param string $guid
+     * @param array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function getGroup(
+        $guid,
+        $parameters = []
+    ) {
+        $path = '/groups/'.$guid;
+        $method = 'GET';
+        
+        return self::sendRequest($path, $method, $parameters);
+    }
+  
+     /**
+     * Update a Group
+     *
+     * @param $guid
+     * @param array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function updateGroup(
+        $guid,
+        $parameters = []
+    ) {
+        $path = '/groups/'.$guid;
+        $method = 'POST';
+        
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Delete a Group
+     *
+     * @param $guid
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function deleteGroup(
+        $guid
+    ) {
+        $path = '/groups/'.$guid;
+        $method = 'DELETE';
+        
+        return self::sendRequest($path, $method);
+    }
+
+    /**
+     * Add Group Members
+     *
+     * @param $guid
+     * @param array  $participants
+     * @param array  $moderators
+     * @param array  $admins
+     * @param array  $usersToBan
+     * @param array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function addGroupMembers(
+        $guid,
+        $participants = [],
+        $moderators = [],
+        $admins = [],
+        $usersToBan = [],
+        $parameters = []
+    ) {
+        $path = '/groups/'.$guid.'/members';
+        $method = 'POST';
+
+        if ($participants) $parameters['participants'] = $participants;
+        if ($moderators) $parameters['moderators'] = $moderators;
+        if ($admins) $parameters['admins'] = $admins;
+        if (usersToBan) $parameters['usersToBan'] = $usersToBan;
+        
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * List Group Members
+     *
+     * @param $guid
+     * @param int    $page
+     * @param array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function listGroupMembers(
+        $guid,
+        $page = 0,
+        $parameters = []
+    ) {
+        $path = '/groups/'.$guid.'/members';
+        $method = 'GET';
+
+        
+        $parameters['page'] = $page;
+        
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Add Group Members
+     *
+     * @param $guid
+     * @param $uid
+     * @param $role
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function updateGroupMemberRole(
+        $guid,
+        $uid,
+        $role // must be participant, moderator, admin
+    ) {
+        $path = '/groups/'.$guid.'/members/'.$uid;
+        $method = 'POST';
+
+        $parameters = [];
+        $parameters['guid'] = $guid;
+        $parameters['uid'] = $uid;
+        $parameters['scope'] = $role;
+      
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Remove Group Members
+     *
+     * @param $guid
+     * @param $uid
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function deleteGroupMember(
+        $guid,
+        $uid
+    ) {
+        $path = '/groups/'.$guid.'/members/'.$uid;
+        $method = 'DELETE';
+
+        return self::sendRequest($path, $method);
+    }
+
+    /**
+     * Create a user
+     *
+     * @param  $guid
+     * @param  null  $name
+     * @param  array  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function createUser(
+        $uid,
+        $name,
+        $parameters = []
+    ) {
+        $path = '/users';
+        $method = 'POST';
+        $parameters['uid'] = $uid;
+        $parameters['name'] = $name;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Update a user
+     *
+     * @param $uid
+     * @param  null  $parameters
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function updateUser(
+        $uid,
+        $parameters = []
+    ) {
+        $path = '/users/'.$uid;
+        $method = 'PUT';
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Delete a user
+     *
+     * @param $uid
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function deleteUser($uid)
+    {
+        $path = '/users/'.$uid;
+        $method = 'DELETE';
+        $parameters['permanent'] = true;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Deactivate a user
+     *
+     * @param $uid
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function deactivateUser($uid)
+    {
+        $path = '/users/'.$uid;
+        $method = 'DELETE';
+        $parameters['permanent'] = false;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Reactivate a user or users
+     *
+     * @param $uids
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function reactivateUser($uids)
+    {
+        $path = '/users';
+        $method = 'PUT';
+        $parameters['uidsToActivate'] = $uids;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Create a user auth token
+     *
+     * @param $uid
+     * @param  bool  $force
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function createToken($uid, $force = false)
+    {
+        $path = '/users/'.$uid.'/auth_tokens';
+        $method = 'POST';
+        $parameters['force'] = $force;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Delete a user auth token
+     *
+     * @param $uid
+     * @param $authToken
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function deleteToken($uid, $authToken)
+    {
+        $path = '/users/'.$uid.'/auth_tokens/'.$authToken;
+        $method = 'DELETE';
+
+        return self::sendRequest($path, $method);
+    }
+
+    /**
+     * Block Users
+     *
+     * @param $uid
+     * @param $blockedUsers
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function blockUsers($uid, $blockedUsers = [])
+    {
+        $path = '/users/'.$uid.'/blockedusers';
+        $method = 'POST';
+        $parameters['blockedUids'] = $blockedUsers;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Unblock Users
+     *
+     * @param $uid
+     * @param $blockedUsers
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function unblockUsers($uid, $blockedUsers = [])
+    {
+        $path = '/users/'.$uid.'/blockedusers';
+        $method = 'DELETE';
+        $parameters['blockedUids'] = $blockedUsers;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Add Friends
+     *
+     * @param $uid
+     * @param $friends
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function addFriends($uid, $friends = [])
+    {
+        $path = '/users/'.$uid.'/friends';
+        $method = 'POST';
+        $parameters['accepted'] = $friends;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Delete Friends
+     *
+     * @param $uid
+     * @param $friends
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function deleteFriends($uid, $friends = [])
+    {
+        $path = '/users/'.$uid.'/friends';
+        $method = 'DELETE';
+        $parameters['friends'] = $friends;
+
+        return self::sendRequest($path, $method, $parameters);
+    }
+
+    /**
+     * Send a request and retrieve the response from the CometChat API
+     *
+     * @param $path
+     * @param $method
+     * @param $data
+     *
+     * @return false|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private static function sendRequest($path, $method, $data = null)
+    {
+        try {
+            $client = new Client();
+            $apiUrl = 'https://'.self::$appId.'.api-'.self::$apiRegion.'.cometchat.io/'.self::$apiVersion.'/';
+
+            $headers = [
+                    'Accept'       => 'application/json',
+                    'Content-Type' => 'application/json',
+                    'apiKey'       => self::$apiKey,
+                ];
+           if (self::$onBehalfOf && (strpos($path, "groups") === 0)) {
+                $headers['onBehalfOf'] = self::$onBehalfOf;
+           }
+
+            $response = $client->request($method, $apiUrl.$path, [
+                'headers' => $headers,
+                'json'    => $data,
+            ]);
+
+            if ($response->getStatusCode() == '200') {
+                $data = (string) $response->getBody();
+
+                return json_decode($data);
+            }
+
+            return false;
+        } catch (RequestException $e) {
+            echo Message::toString($e->getRequest());
+            if ($e->hasResponse()) {
+                echo Message::toString($e->getResponse());
+            }
+        }
+    }
+}
